e8f6651db76702ee9fc097e9d638e5953b7bf89d
Hamid Reza Edalatnejad <hamid.eddie@gmail.com>
2024-10-18 21:01:52 +0330

    verify code completed product model maked

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render , redirect
 from django.contrib import messages
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, VerifyCodeForm
 from random import randint
 from utils import send_otp_code
-from .models import OtpCode
-
+from .models import OtpCode, User
+from datetime import timedelta
+from django.utils import timezone
 class UserRegisterView(View):
     form_class = UserRegistrationForm
+    template_name = 'accounts/register.html'
 
     def get(self, request):
         form = self.form_class
-        return render(request, 'accounts/register.html',{'form': form})
+        return render(request, self.template_name,{'form': form})
 
     def post(self, request):
         form = self.form_class(request.POST)
@@ -27,7 +29,7 @@ class UserRegisterView(View):
             }
             messages.success(request, 'we sent you a code', 'success')
             return redirect('accounts:verify_code')
-        return redirect('home:home')
+        return render(request, self.template_name, {'form': form})
 
 
 
@@ -35,8 +37,58 @@ class UserRegisterView(View):
 
 
 class UserRegisterVerifyCodeView(View):
+    form_class = VerifyCodeForm
     def get(self,request):
-        pass
+        form = self.form_class
+        return render(request,'accounts/verify.html', {'form': form})
 
     def post(self, request):
-        pass
+        user_session = request.session['user_registration_info']
+        code_instance = OtpCode.objects.get(phone_number=user_session['phone_number'])
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            # Calculate two minutes later from the `created` field
+            two_minutes_later = code_instance.created + timedelta(minutes=2)
+            current_time = timezone.now()  # Get the current time
+
+            # Check both conditions: not past two minutes and the code matches
+            if current_time <= two_minutes_later and cd['code'] == code_instance.code:
+                # Create the user if both conditions are true
+                User.objects.create_user(
+                    user_session['phone_number'],
+                    user_session['email'],
+                    user_session['full_name'],
+                    user_session['password']
+                )
+                code_instance.delete()
+                messages.success(request, 'You are registered!', 'success')
+                return redirect('home:home')
+            else:
+                # Handle cases where the code is wrong or has expired
+                if current_time > two_minutes_later:
+                    messages.error(request, 'The code has expired.', 'danger')
+                else:
+                    messages.error(request, 'This code is wrong.', 'danger')
+                return redirect('account:verify_code')
+
+        return redirect('home:home')
+
+
+
+
+# if form.is_valid():
+        #     cd=form.cleaned_data
+        #     two_minutes_later= code_instance.created + timedelta(minutes=2)
+        #     current_time = timezone.now()
+        #     if current_time <= two_minutes_later and cd['code'] == code_instance.code:
+        #         User.objects.create_user(user_session['phone_number'], user_session['email'], user_session['full_name'], user_session['password'])
+        #         code_instance.delete()
+        #         messages.success(request, 'you registered', 'success')
+        #         return redirect('home:home')
+        #     else:
+        #         messages.error(request, 'this code is wrong', 'danger')
+        #         return redirect('account:verify_code')
+        #
+        # return redirect('home:home')
--- utils.py
+++ utils.py
@@ -1,6 +1,18 @@
+from kavenegar import *
 
 
 
 def send_otp_code(phone_number, code):
-    pass
+    try:
+        api = KavenegarAPI('4E425268704F336A472F6B394E4D566D333661654B334638397231414134755541484D7279446E6A786A593D')
+        params = {'sender': '2000500666',
+                  'receptor': phone_number,
+                  'message': f' کد تایید شما {code} '
+        }
+        response = api.sms_send(params)
+        print(response)
+    except APIException as e:
+        print(e)
+    except HTTPException as e:
+        print(e)
 
